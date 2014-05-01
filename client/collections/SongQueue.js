25e8b9a929f5b4880ef048507b51f67ccc244f55
mcreinhard <mcreinhard@gmail.com>
2014-04-30 19:18:15 -0700

    Implemented SongQueue collection

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,35 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+
+  },
+
+  events: {
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  add: function(model) {
+    if (model.length === undefined) model = [model];
+    _.each(model, function(item) {
+      Songs.prototype.add.call(this, item);
+      if (this.length === 1) {
+        this.playFirst();
+      }
+      var added = this.at(this.length - 1);
+      added.on('ended', function () {
+        this.shift();
+        if (this.length) {
+          this.playFirst();
+        }
+      }, this);
+      added.on('dequeue', function() {
+        this.remove(added);
+      },this);
+    }, this);
   }
 
 });
